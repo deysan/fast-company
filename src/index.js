a2b9b98764a48eda9cc4910509457b4de996b8a7
deysan <ideysan@gmail.com>
2021-12-06 21:47:04 +0200

    Add users component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Users from './components/users';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Users />
   </React.StrictMode>,
   document.getElementById('root')
 );
