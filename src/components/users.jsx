cef0a48d9884268bd0d31d5b672ad4aec9c5a958
deysan <ideysan@gmail.com>
2021-12-07 13:54:34 +0200

    Add render user

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api from '../api';
 
 const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+
   console.log(api.users.fetchAll());
+
+  const renderQualitie = (qualities) => {
+    return qualities.map((qualitie) => qualitie.name);
+  };
+
+  const renderUser = () => {
+    return users.map((user) => (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>{renderQualitie(user.qualities)}</td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate} из 5</td>
+        <td>Кнопка удалить</td>
+      </tr>
+    ));
+  };
+
+  const renderTable = () => {
+    return (
+      users.length !== 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>{renderUser()}</tbody>
+        </table>
+      )
+    );
+  };
+
   return (
     <>
-      <span class="badge rounded-pill bg-primary m-2 py-2 px-3 fs-6">
+      <span className="badge rounded-pill bg-primary m-2 py-2 px-3 fs-6">
         # человек тусанет с тобой сегодня
       </span>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Полное имя</td>
-            <td>Массив качеств</td>
-            <td>Название профессии</td>
-            <td>Число</td>
-            <td>Оценка из 5</td>
-            <td>Кнопка удалить</td>
-          </tr>
-        </tbody>
-      </table>
+      {renderTable()}
     </>
   );
 };
