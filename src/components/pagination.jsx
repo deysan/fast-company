a0bd0a151b51858d9618c46de54abf1684c9eb89
deysan <ideysan@gmail.com>
2021-12-12 22:30:46 +0200

    Add pagination page

---
+++ src/components/pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash';
+
+const Pagination = (props) => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  const pageCount = Math.ceil(itemsCount / pageSize);
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+
+  return (
+    <nav aria-label="Page navigation">
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            className={'page-item' + (page === currentPage ? ' active' : '')}
+            key={'page_' + page}
+          >
+            <button className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
