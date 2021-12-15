a9bff24535d08ec873c5524a590d4694bbc376cc
deysan <ideysan@gmail.com>
2021-12-15 19:22:35 +0200

    Add default props

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Filter = ({ items }) => {
+const Filter = ({ items, valueProperty, contentProperty }) => {
   return (
     <ul className="list-group">
       {Object.keys(items).map((item) => (
-        <li key={items[item]._id} className="list-group-item">
-          {items[item].name}
+        <li key={items[item][valueProperty]} className="list-group-item">
+          {items[item][contentProperty]}
         </li>
       ))}
     </ul>
   );
 };
 
+Filter.defaultProps = {
+  valueProperty: '_id',
+  contentProperty: 'name'
+};
+
 Filter.propTypes = {
-  items: PropTypes.object.isRequired
+  items: PropTypes.object.isRequired,
+  valueProperty: PropTypes.string.isRequired,
+  contentProperty: PropTypes.string.isRequired
 };
 
 export default Filter;
