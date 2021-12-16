bc575de3f52c7810e9e91ff428b1a374f7f56917
deysan <ideysan@gmail.com>
2021-12-16 23:37:48 +0200

    Add preloader

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import User from './user';
 import api from '../api';
@@ -101,7 +102,7 @@ const Users = () => {
 
   return (
     <>
-      {users && professions && (
+      {users && professions ? (
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-6 pt-2">
@@ -132,6 +133,12 @@ const Users = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      ) : (
+        <div className="position-absolute top-50 start-50 translate-middle">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Загрузка...</span>
           </div>
         </div>
       )}
