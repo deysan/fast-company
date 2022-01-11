c2b90cee5ca4935728aef5472bb3e7e0a1c955ed
deysan <ideysan@gmail.com>
2022-01-11 18:57:14 +0200

    Add form login and password

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
   return (
     <div className="container">
-      <h1>Login</h1>
+      <h1>Login & Password</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
