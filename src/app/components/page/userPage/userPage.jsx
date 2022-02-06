e6a6ffba3c129deebe1c6b22cc5b8f5153a78c85
deysan <ideysan@gmail.com>
2022-02-06 18:16:59 +0200

    Redesign user page

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -20,36 +20,68 @@ const UserPage = ({ userId }) => {
   if (user) {
     return (
       <div className="container">
-        <div className="card w-50 mt-3">
-          <div className="card-body">
-            <h1 className="card-title">{user.name}</h1>
-            <h3 className="card-subtitle mb-2 text-muted">
-              Профессия: {user.profession.name}
-            </h3>
-            <p className="card-text">
-              <Qualities qualities={user.qualities} />
-            </p>
-            <p className="card-text">
-              Встретился{' '}
-              <span className="fw-bold">{user.completedMeetings}</span> раз
-            </p>
-            <h5 className="card-subtitle mb-2 fw-normal">
-              Оценка: <span className="fw-bold">{user.rate}</span>
-            </h5>
-            <button
-              className="btn btn-warning"
-              type="button"
-              onClick={handleClick}
-            >
-              Изменить данные
-            </button>
-            <div className="mt-4">
-              <Link className="btn btn-secondary" type="button" to="/users">
-                Все пользователи
-              </Link>
+        <div className="row gutters-sm mt-3">
+          <div className="col-md-4">
+            <div className="card mb-3">
+              <div className="card-body">
+                <button
+                  className="position-absolute top-0 end-0 btn btn-light btn-sm"
+                  onClick={handleClick}
+                >
+                  <i className="bi bi-gear"></i>
+                </button>
+                <div className="d-flex flex-column align-items-center text-center position-relative">
+                  <img
+                    src="https://avatars.dicebear.com/api/avataaars/qweqwdas.svg"
+                    className="rounded-circle"
+                    width="150"
+                  />
+                  <div className="mt-3">
+                    <h4>{user.name}</h4>
+                    <p className="text-secondary mb-1">
+                      {user.profession.name}
+                    </p>
+                    <div className="text-muted">
+                      <i
+                        className="bi bi-caret-down-fill text-primary"
+                        role="button"
+                      ></i>
+                      <i
+                        className="bi bi-caret-up text-secondary"
+                        role="button"
+                      ></i>
+                      <span className="ms-2">{user.rate}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="card mb-3">
+              <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                  <span>Qualities</span>
+                </h5>
+                <p className="card-text">
+                  <Qualities qualities={user.qualities} />
+                </p>
+              </div>
+            </div>
+
+            <div className="card mb-3">
+              <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                  <span>Completed meetings</span>
+                </h5>
+                <h1 className="display-1">{user.completedMeetings}</h1>
+              </div>
             </div>
           </div>
         </div>
+
+        <Link className="btn btn-secondary mb-3" type="button" to="/users">
+          Все пользователи
+        </Link>
       </div>
     );
   } else {
