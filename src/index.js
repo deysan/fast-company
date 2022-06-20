5087eee24ec13e39cb1203c26d5739b41393beb0
deysan <ideysan@gmail.com>
2022-06-20 14:56:04 +0300

    Create redux store

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from './app/store/createStore';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
