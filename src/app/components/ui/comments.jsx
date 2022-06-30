d2f7bf2c9c98ab664e47d1e16a3e3dfc9c2cdf5d
deysan <ideysan@gmail.com>
2022-06-30 18:28:36 +0300

    Create comments slice

--- src/app/components/ui/comments.jsx
+++ src/app/components/ui/comments.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CommentForm from '../common/comments/commentForm';
 import CommentsList from '../common/comments/commentsList';
 import { orderBy } from 'lodash';
 import { useComments } from '../../hooks/useComments';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getComments,
+  getCommentsLoadingStatus,
+  loadCommentsList
+} from '../../store/comments';
+import Preloader from './preloader';
+import { useParams } from 'react-router-dom';
 
 const Comments = () => {
-  const { comments, createComment, removeComment } = useComments();
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const { createComment, removeComment } = useComments();
+
+  const comments = useSelector(getComments());
+  const isLoading = useSelector(getCommentsLoadingStatus());
 
   const handleSubmit = (data) => {
     createComment(data);
@@ -17,15 +30,22 @@ const Comments = () => {
 
   const sortedComments = orderBy(comments, ['created_at'], ['desc']);
 
+  useEffect(() => {
+    dispatch(loadCommentsList(userId));
+  }, [userId]);
+
   return (
     <>
       <CommentForm onSubmit={handleSubmit} />
-      {sortedComments.length > 0 && (
-        <CommentsList
-          comments={sortedComments}
-          onDelete={handleDeleteComment}
-        />
-      )}
+      {sortedComments.length > 0 &&
+        (!isLoading ? (
+          <CommentsList
+            comments={sortedComments}
+            onDelete={handleDeleteComment}
+          />
+        ) : (
+          <Preloader />
+        ))}
     </>
   );
 };
